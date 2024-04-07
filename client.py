ff3162667f3e1461aad02305425165e3073a69e8
jzacharia <9064844+jzacharia@users.noreply.github.com>
2024-04-06 23:56:11 -0700

    basic infra for client.py socket interface, added testing section to check if server is recieving

--- client.py
+++ client.py
@@ -1,6 +1,23 @@
+import socket
+
 def main():
     
-
+    # create socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # connect to server
+    s.connect(('localhost', 12000))
+    
+    ###### TESTING SECTION ######
+    ## TESTING DATA TRANSFER TO SERVER
+    s.send("Test Data")
+    ## RECIEVE DATA FROM SERVER
+    response = s.recv(1024)
+    print(response)
+    ###### TESTING SECTION ######
+    
+    s.close()    
+    
     # create functions here to be called by client, acess server.py, and modify values
     # in this case, we want to create functions for users to access the sever and send/recieve transactions of BTC
     
